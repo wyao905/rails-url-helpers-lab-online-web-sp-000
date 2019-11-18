77e5c7270f0f96551e4f09f9d85420d664125eb6
Warren Yao <warrenyca905@gmail.com>
2019-11-18 22:31:24 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+  
+  def activate_student
+    student = set_student
+    if student.active
+      student.active = false
+    else
+      student.active = true
+    end
+    student.save
+    
+    redirect_to "/students/#{student.id}"
   end
 
   private
